22c457f0460c1cd12703c6eb0093e4c1a21a6a7b
Huzaifa Ali <141412821+HuzaifaAliAwan@users.noreply.github.com>
2025-08-01 12:02:46 +0000

    add question 8 module 1 solution

---
+++ Module_2/practice_questions/question8.py
@@ -0,0 +1,21 @@
+# Author : Huzaifa Ali
+# Email : huzaifaa0303@gmail.com
+
+# 8. Multiply two 2x2 matrices.
+
+import numpy as np
+
+def main():
+    try:
+        arr1 = np.random.randint(1,10,(2,2))
+        arr2 = np.random.randint(1,10,(2,2))
+
+        print(f'Array 1 : \n{arr1}')
+        print(f'Array 2 : \n{arr2}')
+
+        print(f'Dot Product : \n{np.dot(arr1, arr2)}')
+    except ValueError as e:
+        print('Invalid Entry, Try Again ..... !')
+
+if __name__ == '__main__':
+    main()
