da4cfbe73ddecef5068cc9b208c40a63d02f0409
HuzaifaAliAwan <141412821+HuzaifaAliAwan@users.noreply.github.com>
2025-08-02 17:16:57 +0500

    add alternate ways to solve same problem

--- Module_2/practice_questions/question8.py
+++ Module_2/practice_questions/question8.py
@@ -13,6 +13,13 @@ def main():
         print(f'Array 1 : \n{arr1}')
         print(f'Array 2 : \n{arr2}')
 
+        # Method 1
+        print(f'Simple Product : \n{arr1 @ arr2}')
+        
+        # Method 2
+        print(f'Mat multiplication : \n{np.matmul(arr1, arr2)}')
+        
+        # Method 3
         print(f'Dot Product : \n{np.dot(arr1, arr2)}')
     except ValueError as e:
         print('Invalid Entry, Try Again ..... !')
